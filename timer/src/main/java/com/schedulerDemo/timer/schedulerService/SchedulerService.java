248a5700b8d58e0aede1f61844f039cf097b5d09
TARUN KUMAR ACHARYA <tarunkumar.acharya4798@gmail.com>
2021-07-18 11:56:26 +0530

    Adding schedule method to service

--- timer/src/main/java/com/schedulerDemo/timer/schedulerService/SchedulerService.java
+++ timer/src/main/java/com/schedulerDemo/timer/schedulerService/SchedulerService.java
@@ -3,10 +3,15 @@ package com.schedulerDemo.timer.schedulerService;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+import org.quartz.JobDetail;
 import org.quartz.Scheduler;
+import org.quartz.Trigger;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.schedulerDemo.timer.configuration.TimerConfig;
+import com.schedulerDemo.timer.utils.TimerUtils;
 
 @Service
 public class SchedulerService {
@@ -36,5 +41,20 @@ public class SchedulerService {
 		} catch (Exception e) {
 			logs.error("Scheduler could not be shutdown e:"+e.getMessage());
 		}
+	}
+	
+	public void schedule(final Class jobClass , final TimerConfig info) {
+		
+		//building a jobDetail first
+		final JobDetail jobDetail = TimerUtils.buildJobDetail(jobClass, info);
+		//building a trigger 
+		final Trigger trigger = TimerUtils.buildTrigger(jobClass, info);
+		
+		try {
+			scheduler.scheduleJob(jobDetail, trigger);
+		} catch (Exception e) {
+			logs.error("Error in triggering scheduler e:"+e.getMessage());
+		}
+		
 	}
 }
