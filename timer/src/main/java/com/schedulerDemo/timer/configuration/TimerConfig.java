3767012fda546332cfb578fd69294a64682d2928
TARUN KUMAR ACHARYA <tarunkumar.acharya4798@gmail.com>
2021-07-18 11:30:00 +0530

    Added config and utils class
    Added config a POJO class , utils class to build JobDetail and the triggers in the schedulers

--- timer/src/main/java/com/schedulerDemo/timer/configuration/TimerConfig.java
+++ timer/src/main/java/com/schedulerDemo/timer/configuration/TimerConfig.java
@@ -8,4 +8,36 @@ public class TimerConfig {
 	private long initialOffsetMs;
 	private String callbackData;
 	
+	public int getTotalFireCount() {
+		return totalFireCount;
+	}
+	public void setTotalFireCount(int totalFireCount) {
+		this.totalFireCount = totalFireCount;
+	}
+	public boolean isRunForever() {
+		return runForever;
+	}
+	public void setRunForever(boolean runForever) {
+		this.runForever = runForever;
+	}
+	public int getRepeatIntervalMs() {
+		return repeatIntervalMs;
+	}
+	public void setRepeatIntervalMs(int repeatIntervalMs) {
+		this.repeatIntervalMs = repeatIntervalMs;
+	}
+	public long getInitialOffsetMs() {
+		return initialOffsetMs;
+	}
+	public void setInitialOffsetMs(long initialOffsetMs) {
+		this.initialOffsetMs = initialOffsetMs;
+	}
+	public String getCallbackData() {
+		return callbackData;
+	}
+	public void setCallbackData(String callbackData) {
+		this.callbackData = callbackData;
+	}
+	
+	
 }
